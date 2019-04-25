afaec7d8507967212a62cb3bae7343338d388bf3
Andrew Duberstein <duberstein@uber.com>
2019-04-25 13:24:59 -0700

    Add Jupyter notebook widget

---
+++ modules/jupyter-widget/src/plugin.js
@@ -0,0 +1,29 @@
+import {Application, IPlugin} from '@phosphor/application';
+
+import {IJupyterWidgetRegistry} from '@jupyter-widgets/base';
+
+import * as widgetExports from './widget';
+
+import {MODULE_NAME, MODULE_VERSION} from './version';
+
+const EXTENSION_ID = 'deckgl-widget:plugin';
+
+const DeckGLPlugin = {
+  id: EXTENSION_ID,
+  requires: [IJupyterWidgetRegistry],
+  activate: activateWidgetExtension,
+  autoStart: true
+};
+
+export default DeckGLPlugin;
+
+/**
+ * Activate the widget extension.
+ */
+function activateWidgetExtension(app, registry) {
+  registry.registerWidget({
+    name: MODULE_NAME,
+    version: MODULE_VERSION,
+    exports: widgetExports
+  });
+}
